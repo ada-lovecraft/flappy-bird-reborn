ce3ca0f39ea02ff4736be908430c884d6617ff64
jdowell <jeremy@codevinsky.com>
2014-04-14 18:43:24 -0500

    README and HTML Update

---
+++ README.md
@@ -0,0 +1,5 @@
+#Flappy Bird Reborn
+The HTML5 Javascript remix built with [Phaser](http://phaser.io) 2.0 and [generator-phaser-official](http://github.com/codevinsky/generator-phaser-official)
+
+## Learn to make this game from the ground up
+[Phaser 2.0 Tutorial: Flappy Bird](http://codevinsky.ghost.io/phaser-2-0-tutorial-flappy-bird-part-1/)
--- game/states/preload.js
+++ game/states/preload.js
@@ -18,8 +18,10 @@ Preload.prototype = {
     this.load.spritesheet('bird', 'assets/bird.png', 34,24,3);
     this.load.spritesheet('pipe', 'assets/pipes.png', 54,320,2);
     this.load.image('startButton', 'assets/start-button.png');
+    
     this.load.image('instructions', 'assets/instructions.png');
     this.load.image('getReady', 'assets/get-ready.png');
+    
     this.load.image('scoreboard', 'assets/scoreboard.png');
     this.load.spritesheet('medals', 'assets/medals.png',44, 46, 2);
     this.load.image('gameover', 'assets/gameover.png');
